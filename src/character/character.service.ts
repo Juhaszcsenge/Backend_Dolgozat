68aca0f8c8dcdac0bd581a66578fe782870b51f2
Juhász Csenge <csenge799@gmail.com>
2023-04-24 16:42:15 +0200

    lekérdezés,módosítás,törlés,hozzáadás validációk

--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -1,28 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
 import { DataSource } from 'typeorm';
+import { Character } from './entities/character.entity';
 
 @Injectable()
 export class CharacterService {
-  constructor( private dataSource: DataSource){}
-  create(createCharacterDto: CreateCharacterDto) {
-    return 'This action adds a new character';
+  constructor(private dataSource: DataSource) {}
+  async create(createCharacterDto: CreateCharacterDto) {
+    const characterRepo = this.dataSource.getRepository(Character);
+    const newCharacter = new Character();
+    newCharacter.name = createCharacterDto.name;
+    newCharacter.experience = createCharacterDto.experience;
+    await characterRepo.save(newCharacter);
+    // return 'This action adds a new character';
   }
 
-  findAll() {
-    return `This action returns all character`;
+  async findAll() {
+    return await this.dataSource.getRepository(Character).find();
+    // return `This action returns all character`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} character`;
   }
 
-  update(id: number, updateCharacterDto: UpdateCharacterDto) {
-    return `This action updates a #${id} character`;
+  async update(id: number, updateCharacterDto: UpdateCharacterDto) {
+    const characterRepo = this.dataSource.getRepository(Character);
+    const characterToUpdate = await characterRepo.findOneBy({ id });
+
+    if (!characterToUpdate) {
+      throw new BadRequestException('Ilyen id-val  nem található karakter');
+    }
+    if (
+      updateCharacterDto.name == null &&
+      updateCharacterDto.experience == null
+    ) {
+      throw new BadRequestException('A kéréshez nem társul semmilyen adat');
+    }
+    characterToUpdate.experience = updateCharacterDto.experience;
+    characterToUpdate.name = updateCharacterDto.name;
+
+    characterRepo.save(characterToUpdate);
+    // return `This action updates a #${id} character`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} character`;
+  async remove(id: number) {
+    const charaterRepo = this.dataSource.getRepository(Character);
+    const charaterDelete = await charaterRepo.findOneBy({ id });
+    if (charaterDelete == null) {
+      throw new BadRequestException();
+    }
+    await charaterRepo.delete({ id: id });
+    // return `This action removes a #${id} character`;
   }
 }
--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreatePlayerDto } from './dto/create-player.dto';
 import { UpdatePlayerDto } from './dto/update-player.dto';
+import { DataSource } from 'typeorm';
+import { Player } from './entities/player.entity';
 
 @Injectable()
 export class PlayerService {
-  create(createPlayerDto: CreatePlayerDto) {
-    return 'This action adds a new player';
+  constructor(private dataSource: DataSource) {}
+
+  async create(createPlayerDto: CreatePlayerDto) {
+    const playerRepo = this.dataSource.getRepository(Player);
+    const newPlayer = new Player();
+    newPlayer.email = createPlayerDto.email;
+    newPlayer.banned = createPlayerDto.banned;
+    await playerRepo.save(newPlayer);
+    // return 'This action adds a new player';
   }
 
-  findAll() {
-    return `This action returns all player`;
+  async findAll() {
+    // return `This action returns all player`;
+    return await this.dataSource.getRepository(Player).find();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} player`;
   }
 
-  update(id: number, updatePlayerDto: UpdatePlayerDto) {
-    return `This action updates a #${id} player`;
+  async update(id: number, updatePlayerDto: UpdatePlayerDto) {
+    const playerRepo = this.dataSource.getRepository(Player);
+    const playerToUpdate = await playerRepo.findOneBy({ id });
+
+    if (!playerToUpdate) {
+      throw new BadRequestException('Ilyen id-val nem található játékos');
+    }
+    if (updatePlayerDto.email == null && updatePlayerDto.banned == null) {
+      throw new BadRequestException(
+        'A kéréshez nem társult semilyen email cím',
+      );
+    }
+    playerToUpdate.email = updatePlayerDto.email;
+    playerToUpdate.banned = updatePlayerDto.banned;
+
+    playerRepo.save(playerToUpdate);
+    // return `This action updates a #${id} player`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} player`;
+  async remove(id: number) {
+    // return `This action removes a #${id} player`;
+    const playerRepo = this.dataSource.getRepository(Player);
+    const playerToDelete = await playerRepo.findOneBy({ id });
+    if (playerToDelete == null) {
+      throw new BadRequestException();
+    }
+    return await playerRepo.delete({ id: id });
   }
 }
